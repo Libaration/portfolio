43a154ceb29e1254a19ecd636e0968e4f7b3f30b
Cristian Deleon <libaration@gmail.com>
2021-09-11 00:37:11 -0400

    adds iframe to habbohtml5 project in details section

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -7,20 +7,24 @@ const Details = forwardRef<HTMLDivElement, Props>((props, ref) => {
   return (
     <div className="details" ref={ref}>
       <Habbo />
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Laoreet id donec
-      ultrices tincidunt arcu non. Pharetra massa massa ultricies mi quis
-      hendrerit. Platea dictumst vestibulum rhoncus est pellentesque elit. Arcu
-      non odio euismod lacinia. Arcu vitae elementum curabitur vitae nunc sed
-      velit dignissim. Tortor pretium viverra suspendisse potenti nullam ac. Ut
-      consequat semper viverra nam libero justo laoreet sit. Tristique senectus
-      et netus et malesuada fames ac turpis egestas. A pellentesque sit amet
-      porttitor eget dolor morbi non arcu. Lectus urna duis convallis convallis
-      tellus. Tortor pretium viverra suspendisse potenti nullam ac. Ultrices
-      eros in cursus turpis massa tincidunt dui. Dictum varius duis at
-      consectetur lorem donec massa sapien faucibus. Dui sapien eget mi proin
-      sed libero enim. At erat pellentesque adipiscing commodo elit at imperdiet
-      dui. Gravida in
+      <div className="detailsParagraph">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet id
+          donec ultrices tincidunt arcu non. Pharetra massa massa ultricies mi
+          quis hendrerit. Platea dictumst vestibulum rhoncus est pellentesque
+          elit. Arcu non odio euismod lacinia. Arcu vitae elementum curabitur
+          vitae nunc sed velit dignissim. Tortor pretium viverra suspendisse
+          potenti nullam ac. Ut consequat semper viverra nam libero justo
+          laoreet sit. Tristique senectus et netus et malesuada fames ac turpis
+          egestas. A pellentesque sit amet porttitor eget dolor morbi non arcu.
+          Lectus urna duis convallis convallis tellus. Tortor pretium viverra
+          suspendisse potenti nullam ac. Ultrices eros in cursus turpis massa
+          tincidunt dui. Dictum varius duis at consectetur lorem donec massa
+          sapien faucibus. Dui sapien eget mi proin sed libero enim. At erat
+          pellentesque adipiscing commodo elit at imperdiet dui. Gravida in
+        </p>
+      </div>
     </div>
   );
 });
--- src/components/Habbo.tsx
+++ src/components/Habbo.tsx
@@ -4,11 +4,31 @@ interface Props {}
 
 const Habbo = (props: Props) => {
   return (
-    <div className="habboClient">
-      <iframe
-        src="https://habbo-html-5-8wdzf.ondigitalocean.app"
-        title="client"
-      />
+    <div className="project">
+      <div className="habboClient">
+        <iframe
+          className="clientIframe"
+          src="https://habbo-html-5-zvos9.ondigitalocean.app/"
+          title="client"
+          scrolling="no"
+        />
+      </div>
+      <div className="projectDescription">
+        <b>Project:</b> HabboHTML5
+        <br />
+        <b>Language:</b> Vanilla JS
+        <br />
+        <b>Objective:</b> Recreate childhood game originally coded in
+        Shockwave/Flash to HTML5 and JS
+        <br />
+        <b>Tech:</b> Uses Phaser3 library for rendering canvas / game sprites
+        and Tiled to create Tilemaps and textures. Implements EasyStar package
+        for pathfinding
+        <br />
+        <a href="https://github.com/Libaration/HabboHTML5">
+          <b>GitHub Source</b>
+        </a>
+      </div>
     </div>
   );
 };
