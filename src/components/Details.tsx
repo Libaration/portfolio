fa4364f67195b7a1a866aa0dd30c434c8cda0cae
Cristian Deleon <libaration@gmail.com>
2021-09-11 01:03:35 -0400

    adds to about/details section

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -8,21 +8,22 @@ const Details = forwardRef<HTMLDivElement, Props>((props, ref) => {
     <div className="details" ref={ref}>
       <Habbo />
       <div className="detailsParagraph">
+        <h1> It all began here!</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Laoreet id
-          donec ultrices tincidunt arcu non. Pharetra massa massa ultricies mi
-          quis hendrerit. Platea dictumst vestibulum rhoncus est pellentesque
-          elit. Arcu non odio euismod lacinia. Arcu vitae elementum curabitur
-          vitae nunc sed velit dignissim. Tortor pretium viverra suspendisse
-          potenti nullam ac. Ut consequat semper viverra nam libero justo
-          laoreet sit. Tristique senectus et netus et malesuada fames ac turpis
-          egestas. A pellentesque sit amet porttitor eget dolor morbi non arcu.
-          Lectus urna duis convallis convallis tellus. Tortor pretium viverra
-          suspendisse potenti nullam ac. Ultrices eros in cursus turpis massa
-          tincidunt dui. Dictum varius duis at consectetur lorem donec massa
-          sapien faucibus. Dui sapien eget mi proin sed libero enim. At erat
-          pellentesque adipiscing commodo elit at imperdiet dui. Gravida in
+          My love for coding began in 5th grade. I wanted virtual currency for
+          an online game {'(Recreation shown left screen)'}. When my parents
+          refused I googled relentlessly trying to find how to get this currency
+          for free. Unfortunately, all I got from that was viruses! Through
+          trial and error however, I was lead to online communities who modded
+          the game through HEX editing and packet sniffing/logging! I began
+          learning Visual Basic 6 and contributing basic{'(no pun intended)'}
+          applications which lead to learning HTML and PHP to have a place to
+          host my applications!
+          <br />I recently attended Flatiron and completed the Full Stack
+          developer program where my attention switched from Desktop to Web and
+          am enjoying it just as much as desktop development! I love problem
+          solving and creating projects to really gain a deeper understanding in
+          whatever I create.
         </p>
       </div>
     </div>
